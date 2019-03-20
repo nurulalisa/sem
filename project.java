a9d65924ba8e21d9e76e4187c7181137f6b18a52
nurulalisa <48412763+nurulalisa@users.noreply.github.com>
2019-03-20 22:30:31 +0800

    Update project.java

--- project.java
+++ project.java
@@ -1,1 +1,35 @@
-yuafvsauvaukjhdagfvsaaufgafvavlvhafvaofy
+import java.io.*;
+import java.net.*;
+public class server
+{
+public static void main(String[] args)throws Exception
+{
+ServerSocket serversocket = new ServerSocket(8086);
+System.out.println("Fuwuqi zhunbei haole");
+Socket sock = serversocket.accept();
+System.out.println("ni hao");
+
+BufferedReader keyRead = new BufferedReader(new 
+InputStreamReader(System.in));
+OutputStream ostream = sock.getOutputStream();
+PrintWriter pwrite = new PrintWriter(ostream,true);
+
+InputStream istream = sock.getInputStream();
+BufferedReader receiveRead = new BufferedReader(new 
+InputStreamReader(istream));
+
+String receiveMessage,sendMessage;
+while(true)
+{
+if((receiveMessage=receiveRead.readLine())!=null)
+{
+System.out.println(receiveMessage);
+}
+sendMessage = keyRead.readLine();
+pwrite.println(sendMessage);
+pwrite.flush();
+}
+}
+}
+    
+  
